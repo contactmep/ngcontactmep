ed86cc7f255ad99f287ef78510b2636faac7ea86
belgac <hans.scheuren@bizliner.com>
2015-06-07 14:09:03 +0000

    add gulp copy task

---
+++ gulp/tasks/dev/copy.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+var config = require('../../config').copy.dev;
+
+gulp.task('copy:dev',function(callback) {
+   runSequence(['copy:dev:localjs','copy:dev:templates'],callback);
+})
+
+gulp.task('copy:dev:localjs',['jslint:dev'],function(){
+  return gulp.src(config.localjs.src)
+  .pipe(gulp.dest(config.localjs.dest))
+})
+
+gulp.task('copy:dev:templates',function(){
+  return gulp.src(config.templates.src)
+  .pipe(gulp.dest(config.templates.dest));
+})
