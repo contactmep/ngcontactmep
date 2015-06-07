233ebb98b0074bda67dc567f6b99764683084bad
belgac <hans.scheuren@bizliner.com>
2015-06-07 15:09:51 +0000

    add minify and uglify to usemin gulp task

--- gulp/tasks/dist/usemin.js
+++ gulp/tasks/dist/usemin.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var usemin = require('gulp-usemin');
 var rev = require('gulp-rev');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
 var config = require('../../config').usemin;
 
 gulp.task('usemin', function () {
   return gulp.src(config.src)
     .pipe(usemin({
-    vendorJS:['concat',rev()],
-    appJS:['concat',rev()],
-    vendorCss:['concat',rev()]
+    vendorJS:['concat',uglify(),rev()],
+    appJS:['concat',uglify(),rev()],
+    vendorCss:['concat',minifyCss(),rev()]
   }))
     .pipe(gulp.dest(config.dest));
 })
