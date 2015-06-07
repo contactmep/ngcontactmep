404460f1ecd4af028b95962676e158d5e6c9bbea
belgac <hans.scheuren@bizliner.com>
2015-06-07 15:05:59 +0000

    add rev number managment in usemin gulp task

--- gulp/tasks/dist/usemin.js
+++ gulp/tasks/dist/usemin.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
 var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
 
 var config = require('../../config').usemin;
 
 gulp.task('usemin', function () {
   return gulp.src(config.src)
     .pipe(usemin({
-    vendorJS:['concat'],
-    appJS:['concat'],
-    vendorCss:['concat']
+    vendorJS:['concat',rev()],
+    appJS:['concat',rev()],
+    vendorCss:['concat',rev()]
   }))
     .pipe(gulp.dest(config.dest));
 })
