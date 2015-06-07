d5c3f8c89ef5758afea732e744ebbb62d51abd76
belgac <hans.scheuren@bizliner.com>
2015-06-07 14:25:10 +0000

    correct jslint gulp task name in copy

--- gulp/tasks/dev/copy.js
+++ gulp/tasks/dev/copy.js
@@ -7,7 +7,7 @@ gulp.task('copy:dev',function(callback) {
    runSequence(['copy:dev:localjs','copy:dev:templates'],callback);
 })
 
-gulp.task('copy:dev:localjs',['jslint:dev'],function(){
+gulp.task('copy:dev:localjs',['jslint'],function(){
   return gulp.src(config.localjs.src)
   .pipe(gulp.dest(config.localjs.dest))
 })
