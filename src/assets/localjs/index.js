634f3777532747f2f41bd46af3a3fed8c0dac4a2
belgac <hans.scheuren@bizliner.com>
2015-06-09 09:56:04 +0000

    ident angular index.js

--- src/assets/localjs/index.js
+++ src/assets/localjs/index.js
@@ -1,59 +1,59 @@
-angular.module('ngContactMep',['ui.router','ngContactMep.homeController'])
-  .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+angular.module('ngContactMep', ['ui.router', 'ngContactMep.homeController'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home',{
-      url:'/',
-      views:{
-        main:{
-         templateUrl:'./assets/localjs/home/home.view.html',
+      .state('home', {
+        url: '/',
+        views: {
+          main: {
+            templateUrl: './assets/localjs/home/home.view.html'
+          }
         }
-      }
-    })
-      .state('meps',{
-        abstract:true,
-        views:{
-          main:{
+      })
+      .state('meps', {
+        abstract: true,
+        views: {
+          main: {
             templateUrl: './assets/localjs/meps/meps.view.html'
-          } 
-        }
-      })
-      .state('meps.list',{
-        url:'/meps',
-        views:{
-          meps:{
-            templateUrl:'./assets/localjs/meps/list/mepsList.view.html'
           }
         }
       })
-      .state('meps.mep',{
-        url:'/meps/:id',
-        views:{
-          meps:{
-            templateUrl:'./assets/localjs/meps/mep/mep.view.html'
+      .state('meps.list', {
+        url: '/meps',
+        views: {
+          meps: {
+            templateUrl: './assets/localjs/meps/list/mepsList.view.html'
           }
         }
       })
-      .state('campaigns',{
-        abstract:true,
-        views:{
-          main:{
+      .state('meps.mep', {
+        url: '/meps/:id',
+        views: {
+          meps: {
+            templateUrl: './assets/localjs/meps/mep/mep.view.html'
+          }
+        }
+      })
+      .state('campaigns', {
+        abstract: true,
+        views: {
+          main: {
             templateUrl: './assets/localjs/campaigns/campaigns.view.html'
           }
         }
       })
-    .state('campaigns.list',{
-        url:'/campaigns',
-        views:{
-          campaigns:{
-            templateUrl:'./assets/localjs/campaigns/list/campaignsList.view.html'
+      .state('campaigns.list', {
+        url: '/campaigns',
+        views: {
+          campaigns: {
+            templateUrl: './assets/localjs/campaigns/list/campaignsList.view.html'
           }
         }
       })
-      .state('campaigns.campaign',{
-        url:'/campaigns/:id',
-        views:{
-          campaigns:{
-            templateUrl:'./assets/localjs/campaigns/campaign/campaign.view.html'
+      .state('campaigns.campaign', {
+        url: '/campaigns/:id',
+        views: {
+          campaigns: {
+            templateUrl: './assets/localjs/campaigns/campaign/campaign.view.html'
           }
         }
       });
