08ac6f21a43daa7e4d41eb8f5effb502b55953fc
Hans Scheuren <hans.scheuren@bizliner.com>
2015-06-11 11:15:41 +0200

    abandon manual massaging

--- gulp/tasks/dev/datamassage.js
+++ gulp/tasks/dev/datamassage.js
@@ -2,26 +2,15 @@ var gulp = require('gulp');
 var remoteSrc = require('gulp-remote-src');
 var rename = require("gulp-rename");
 var replace = require('gulp-replace');
-var closingTag = /<\/(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
 gulp.task('datamassage', function() {
 
-  remoteSrc(['en/xml.html','fr/xml.html'], {
-      base: 'http://www.europarl.europa.eu/meps/',
+  remoteSrc(['en/json/getBodyValues.html','fr/json/getBodyValues.html'], {
+      base: 'http://www.europarl.europa.eu/meps/'
     })
-    .pipe(replace('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>', ''))
-    .pipe(replace('<meps>', '['))
-    .pipe(replace('</meps>', ']'))
-    .pipe(replace('<mep>', '{'))
-    .pipe(replace('</mep>', '},'))
-    .pipe(replace('"', ''))
-    .pipe(replace(closingTag, '",'))
-    .pipe(replace('<', '"'))
-    .pipe(replace('>', '":"'))
-    .pipe(replace(',}', '}'))
-    .pipe(replace(',]', ']'))
     .pipe(rename(function(path) {
-      path.basename = "mepslist";
-      path.extname = ".json"
+    	path.dirname = path.dirname.replace('json/','');
+      	path.basename = "mepslist";
+      	path.extname = ".json"
     }))
     .pipe(gulp.dest('./app/dev/assets/data/meps'));
 })
