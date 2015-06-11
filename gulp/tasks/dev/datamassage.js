0c6651252dbb88f9b6b541d8381f8ddb97619e45
Hans Scheuren <hans.scheuren@bizliner.com>
2015-06-11 11:58:04 +0200

    Go back to data massaging
    Json data massaging from xml was abandoned because of the availibility
    of a direct json source. This source can't be exploited we go back to
    data massaging

--- gulp/tasks/dev/datamassage.js
+++ gulp/tasks/dev/datamassage.js
@@ -2,15 +2,36 @@ var gulp = require('gulp');
 var remoteSrc = require('gulp-remote-src');
 var rename = require("gulp-rename");
 var replace = require('gulp-replace');
+
+var closingTag = /<\/(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
+
+var langs = ['fr','en','es'];
+
+var remoteSource = [];
+
+for (lang in langs){
+	remoteSource.push(langs[lang] + '/json/getBodyValues.html')
+}
+
 gulp.task('datamassage', function() {
 
-  remoteSrc(['en/json/getBodyValues.html','fr/json/getBodyValues.html'], {
-      base: 'http://www.europarl.europa.eu/meps/'
+  remoteSrc(remoteSource, {
+      base: 'http://www.europarl.europa.eu/meps/',
     })
+    .pipe(replace('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>', ''))
+    .pipe(replace('<meps>', '['))
+    .pipe(replace('</meps>', ']'))
+    .pipe(replace('<mep>', '{'))
+    .pipe(replace('</mep>', '},'))
+    .pipe(replace('"', ''))
+    .pipe(replace(closingTag, '",'))
+    .pipe(replace('<', '"'))
+    .pipe(replace('>', '":"'))
+    .pipe(replace(',}', '}'))
+    .pipe(replace(',]', ']'))
     .pipe(rename(function(path) {
-    	path.dirname = path.dirname.replace('json/','');
-      	path.basename = "mepslist";
-      	path.extname = ".json"
+      path.basename = "mepslist";
+      path.extname = ".json"
     }))
     .pipe(gulp.dest('./app/dev/assets/data/meps'));
 })
