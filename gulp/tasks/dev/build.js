6a2857d407a94259f57fa8c2aa13a7001bba71e5
Hans Scheuren <hans.scheuren@bizliner.com>
2015-06-19 10:35:37 +0200

    add datamassage to gulp builds

--- gulp/tasks/dev/build.js
+++ gulp/tasks/dev/build.js
@@ -5,7 +5,7 @@ var runSequence = require('run-sequence');
 gulp.task('build:dev', function(callback) {
   runSequence(
     'delete:dev',
-    ['bower','copy:dev','jade'],
+    ['bower','copy:dev','jade','datamassage'],
     'inject',
     callback
   );
