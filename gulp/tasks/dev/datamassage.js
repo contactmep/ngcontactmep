cb8f6644d9d7c8ca75e9088f6271c8f736c18e5a
Hans Scheuren <hans.scheuren@bizliner.com>
2015-06-19 10:09:07 +0200

    Finish datamassage task

--- gulp/tasks/dev/datamassage.js
+++ gulp/tasks/dev/datamassage.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var remoteSrc = require('gulp-remote-src');
 var rename = require("gulp-rename");
 var replace = require('gulp-replace');
 var extractMepData = require('../../utilities/extractMepData');
+
+//LOCAL CONFIG
 
 var closingTag = /<\/(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
 
@@ -14,9 +17,23 @@ for (lang in langs){
 	remoteSource.push(langs[lang] + '/xml.html')
 }
 
-gulp.task('datamassage', function() {
+//PATH CONFIG
 
-  remoteSrc(remoteSource, {
+var pathConfig = require('../../config.json');
+
+var mepsDataDest = pathConfig.testBuild.dev 
+    + pathConfig.assets 
+    + pathConfig.dataDir 
+    + '/meps';
+
+//TASK
+
+gulp.task('datamassage', function(callback) {
+    runSequence(['datamassage:mep'],callback)
+})
+
+gulp.task('datamassage:mep', function() {
+  return remoteSrc(remoteSource, {
       base: 'http://www.europarl.europa.eu/meps/',
     })
     .pipe(replace('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>', ''))
@@ -35,5 +52,5 @@ gulp.task('datamassage', function() {
       path.extname = ".json"
     }))
     .pipe(extractMepData())
-    .pipe(gulp.dest('./app/dev/assets/data/meps'));
+    .pipe(gulp.dest(mepsDataDest));
 })
